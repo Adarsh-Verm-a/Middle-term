4c527e4b977c66c114e1c8ac0011d9924369e391
ACE <vadarsh717@gmail.com>
2024-07-13 11:34:56 +0530

    Version 1.0

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+        document.querySelector(".mt").addEventListener("click", ()=>{
+            let a = document.querySelector(".a").value;
+            let b = document.querySelector(".b").value;
+            let c = document.querySelector(".c").value;
+            let middleTerm1 = (-b + Math.sqrt(b*b - 4*a*c)) / (2*a);
+        let middleTerm2 = (-b - Math.sqrt(b*b - 4*a*c)) / (2*a);  
+        console.log(-middleTerm1);
+        console.log(-middleTerm2);   
+        document.querySelector(".result1").value= (-middleTerm1) +"+"+ (-middleTerm2)+"="+b;  
+        document.querySelector(".result2").value= (-middleTerm1) +"*"+ (-middleTerm2)+"="+a*c;  
 
-        let middleTerm1 = (-b + Math.sqrt(b*b - 4*a*c)) / (2*a);
-        let middleTerm2 = (-b - Math.sqrt(b*b - 4*a*c)) / (2*a);     
-       
-   
-        let button = document.querySelector(".mt");
-        button.addEventListener("click", () => {
-            console.log("gay");
         });
-        
